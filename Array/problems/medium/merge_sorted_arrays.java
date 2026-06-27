7aa3ad55b3b129bd505f3969c70632aa9b1baf23
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-06-27 19:36:39 +0530

    feat: add merge sorted arrays

---
+++ Array/problems/medium/merge_sorted_arrays.java
@@ -0,0 +1,35 @@
+// Problem: Merge Two Sorted Arrays
+// Approach: Two Pointers
+// Time Complexity: O(n + m)
+// Space Complexity: O(n + m)
+
+class Solution {
+
+    public int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+
+        int[] merged = new int[arr1.length + arr2.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+
+            if (arr1[i] <= arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+
+        while (i < arr1.length) {
+            merged[k++] = arr1[i++];
+        }
+
+        while (j < arr2.length) {
+            merged[k++] = arr2[j++];
+        }
+
+        return merged;
+    }
+}
