8fa55b6117ab74eb777f9533e69c646246907e8a
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-16 14:57:06 +0530

    feat: add empty string check

---
+++ String/problems/easy/check_empty_string.java
@@ -0,0 +1,12 @@
+// Problem: Check if String is Empty
+// Approach: Built-in Method
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+class Solution {
+
+    public boolean isEmptyString(String str) {
+
+        return str.isEmpty();
+    }
+}
