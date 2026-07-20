979ea912796bddf492e5ecdccdacd0ac8fc55aa1
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-20 18:28:30 +0530

    feat: add regular expression matching

---
+++ String/problems/hard/regular_expression_matching.java
@@ -0,0 +1,52 @@
+// Problem: Regular Expression Matching
+// Approach: Dynamic Programming
+// Time Complexity: O(m * n)
+// Space Complexity: O(m * n)
+
+class Solution {
+
+    public boolean isMatch(String str, String pattern) {
+
+        int m = str.length();
+        int n = pattern.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[0][0] = true;
+
+        for (int j = 2; j <= n; j++) {
+
+            if (pattern.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                char currentPattern = pattern.charAt(j - 1);
+
+                if (currentPattern == '.' ||
+                    currentPattern == str.charAt(i - 1)) {
+
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                } else if (currentPattern == '*') {
+
+                    dp[i][j] = dp[i][j - 2];
+
+                    char previousPattern = pattern.charAt(j - 2);
+
+                    if (previousPattern == '.' ||
+                        previousPattern == str.charAt(i - 1)) {
+
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
