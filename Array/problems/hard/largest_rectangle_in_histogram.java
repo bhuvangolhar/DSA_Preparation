bbb83de04794626eec1f03866acd3e807341dcc5
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-21 16:41:07 +0530

    feat: add largest rectangle in histogram

---
+++ Array/problems/hard/largest_rectangle_in_histogram.java
@@ -0,0 +1,50 @@
+// Problem: Largest Rectangle in Histogram
+// Approach: Monotonic Stack
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+import java.util.Stack;
+
+class Solution {
+
+    public int largestRectangleArea(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int maxArea = 0;
+
+        for (int i = 0; i <= heights.length; i++) {
+
+            int currentHeight =
+                    (i == heights.length)
+                    ? 0
+                    : heights[i];
+
+            while (!stack.isEmpty() &&
+                   currentHeight < heights[stack.peek()]) {
+
+                int height = heights[stack.pop()];
+
+                int width;
+
+                if (stack.isEmpty()) {
+
+                    width = i;
+
+                } else {
+
+                    width = i - stack.peek() - 1;
+                }
+
+                maxArea = Math.max(
+                        maxArea,
+                        height * width
+                );
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
